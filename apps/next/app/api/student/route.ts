ac294b3828b9793acb7302efbed673987db9b0d9
ayaanqasmi <ayaan.qasmi@gmail.com>
2024-08-27 03:40:46 +0500

    student register/login

--- apps/next/app/api/student/route.ts
+++ apps/next/app/api/student/route.ts
@@ -1,6 +1,8 @@
 import db from "lib/mongo";
 import { NextRequest, NextResponse } from "next/server";
 import Student from "models/student"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
 export const GET=async(req:NextRequest)=>{
     await db.dbConnect();
     const allstudents=await Student.find()
