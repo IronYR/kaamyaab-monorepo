9f772dbc8c8c2cd237e0ef48b447bb575df06cb9
ayaanqasmi <ayaan.qasmi@gmail.com>
2024-09-17 20:31:14 +0500

    auth

--- apps/next/app/api/auth/login/route.ts
+++ apps/next/app/api/auth/login/route.ts
@@ -1,11 +1,14 @@
 import db from 'lib/mongo'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
+import Student from 'models/student'
 import Recruiter from 'models/recruiter'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
+import {serialize} from 'cookie'
 
 
-export const POST = async (req: NextRequest) => {
+export const POST = async (req: Request) => {
+    const maxTime=60*20
     try{
         
         await db.dbConnect()
@@ -14,7 +17,15 @@ export const POST = async (req: NextRequest) => {
             return NextResponse.json({ msg: 'enter all fields dimwit' }, { status: 400 })
         }
 
-        const user=await Recruiter.findOne({email})
+        let user=await Student.findOne({email})
+        let role
+        if(!user){
+            user=await Recruiter.findOne({email})
+            role="recruiter"
+        }
+        else{
+            role="student"
+        }
         if(!user){
             return NextResponse.json({ msg: 'no user found buffoon' }, { status: 400 })
         }
@@ -30,12 +41,22 @@ export const POST = async (req: NextRequest) => {
                 username: user.username,
                 email: user.email,
                 id: user.id,
-                role:"recruiter"
+                role:role
               },
             },
-            secret,{expiresIn:"2m"}
+            secret,{expiresIn:maxTime}
         );
-        return NextResponse.json({ token:accessToken }, { status: 201 })
+
+        const serialized = serialize("test", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: maxTime,
+            path: "/",
+          });
+
+        const res={token:accessToken, serialize:serialized}
+        return new Response(JSON.stringify(res),{status:200, headers:{'Set-Cookie':serialized},})
 
     }
     catch(err){
